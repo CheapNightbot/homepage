db72d1cf529b68f2f6c58dee3420f30f078bd5bb
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-21 08:27:34 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
+import Shelf from "@/components/shelf"
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Shelf />
+        </ThemeProvider>
+    )
 }
 
 export default App
