81debe98d69e1a8049fa9cd4729d9b603bdd7a85
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-23 14:10:43 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import Shelf from "@/components/shelf"
 import Window from "@/components/window"
+import { apps } from "@/lib/apps";
+import { projects } from "@/lib/projects";
 
 function App() {
+    const appList = [...projects, ...apps];
+
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <Shelf />
-            <Window></Window>
+            <main className="relative h-[calc(100vh-4rem)]">
+                <Window>
+                    <div className="flex flex-col items-center justify-center h-full text-2xl text-center text-muted-foreground">
+                        <p>Welcome to my portfolio!</p>
+                        <p>Click the launcher button on the left to see my projects and apps.</p>
+                        <p>Or press <code>Ctrl + A</code> to open the launcher.</p>
+                    </div>
+                </Window>
+            </main>
+            <Shelf appList={appList} />
         </ThemeProvider>
     )
 }
