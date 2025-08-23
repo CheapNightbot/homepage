0a660eef6d589b3c2a9b610ab266145ac8d184eb
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-23 09:23:27 +0900

    Create apps.ts

---
+++ src/lib/apps.ts
@@ -0,0 +1,32 @@
+
+export const apps = [
+    {
+        id: "7d7cb976-b629-560e-b427-0d2f33602908",
+        name: "Blog",
+        description: "Here will write some stuffy ~ ?",
+        techStack: [
+            "Vite",
+            "React",
+            "Tailwindcss",
+            "TypeScript",
+            "CloudFlare Workers",
+        ],
+        image: "",
+        github: "https://github.com/CheapNightbot/homepage",
+        live: "https://yutify.cheapnightbot.me/blog"
+    },
+    {
+        id: "1b166866-80b5-5a9f-a272-2c559186fe0c",
+        name: "Journal",
+        description: "hohoi ~ welcome, me Cheap Nightbot (a boiled potato !!), and this is my corner of the internet where I share my thoughts and daily ins & outs.",
+        techStack: [
+            "HTML",
+            "JavaScript",
+            "CSS",
+            "zola"
+        ],
+        image: "",
+        github: "https://github.com/CheapNightbot/Journal",
+        live: "https://journal.cheapnightbot.me"
+    },
+]
