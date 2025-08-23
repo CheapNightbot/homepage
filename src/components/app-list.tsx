a2740ce1d7cf485ad306283bf6d38b12770a4a38
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-23 10:20:04 +0900

    Move AppIcon to its own component

--- src/components/app-list.tsx
+++ src/components/app-list.tsx
@@ -1,22 +1,7 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { apps } from "@/lib/apps";
 import { projects } from "@/lib/projects";
 
-function AppIcon({ icon, name }: { icon?: string; name: string; }) {
-  return (
-    <Button
-      className="size-18 flex flex-col gap-0 pb-4 hover:!bg-transparent items-center justify-center text-primary/70 hover:text-primary"
-      variant="ghost"
-    >
-      <Avatar className="size-14 p-1.5">
-        <AvatarImage src={icon} />
-        <AvatarFallback className="text-2xl">{name.charAt(0)}</AvatarFallback>
-      </Avatar>
-      <span className="text-xs text-center">{name}</span>
-    </Button>
-  );
-}
+import AppIcon from "@/components/app-icon";
 
 function AppList() {
   const appList = [...projects, ...apps]
