e2b7049931e9c570f6bddb9fe8b69a10563e479a
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-06 08:54:41 +0900

    Create top-bar.tsx

---
+++ src/components/top-bar.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle"
+import { SITE_TITLE } from "@/constants";
+
+function TopBar() {
+    return (
+        <header className="fixed top-0 left-0 w-full h-8 bg-card/20 backdrop-blur-md flex drop-shadow-md items-center justify-between px-4">
+            <h1 className="text-sm font-semibold text-primary dark:text-primary select-none">{SITE_TITLE}</h1>
+            <ModeToggle />
+        </header>
+    );
+}
+
+export default TopBar;
