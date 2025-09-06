515aa46c7299578a87b27ddabd2380aa4c1620f0
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-06 09:06:22 +0900

    Update mode-toggle.tsx

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -6,9 +6,9 @@ export function ModeToggle() {
     const { theme, setTheme } = useTheme()
 
     return (
-        <Button onClick={() => setTheme(theme === "light" ? "dark" : "light")} variant="default" size="icon" className="rounded-full size-5 !bg-transparent w-fit h-fit focus-visible:ring-0">
-            <Sun className="h-[1rem] w-[1rem] text-primary scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-            <Moon className="absolute h-[1rem] w-[1rem] text-white scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <Button onClick={() => setTheme(theme === "light" ? "dark" : "light")} variant="default" size="icon" className="rounded-full size-5 text-white/85 hover:text-white !bg-transparent w-fit h-fit focus-visible:ring-0">
+            <Sun className="h-[1rem] w-[1rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+            <Moon className="absolute h-[1rem] w-[1rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
             <span className="sr-only">Toggle theme</span>
         </Button>
     )
