fe7a21d9d0c2ac8e66b687dc79f539ce3ec96204
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-06 11:27:19 +0900

    Add Loading Screen until all resources load

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import Shelf from "@/components/shelf";
 import TopBar from "@/components/top-bar";
 import { ThemeProvider } from "@/components/theme-provider";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const handleLoad = () => setLoading(false);
+        window.addEventListener("load", handleLoad);
+
+        return () => { window.removeEventListener("load", handleLoad) };
+    }, []);
 
     return (
         <ThemeProvider defaultTheme="dark" storageKey="potato-ui-theme">
+            {loading && <LoadingScreen />}
             <main className="w-screen h-screen">
                 <TopBar />
                 <Shelf />
---
+++ src/components/loading-screen.tsx
@@ -0,0 +1,21 @@
+import { Loader2 } from "lucide-react";
+
+function LoadingScreen() {
+    return (
+        <div className="w-screen h-screen fixed top-0 left-0 bg-[#fefcff] flex items-center justify-center z-50 animate-out fade-out duration-200 ease-out">
+            <div
+                className="absolute inset-0 z-0"
+                style={{
+                    backgroundImage: `
+                    radial-gradient(circle at 30% 70%, rgba(173, 216, 230, 0.35), transparent 60%),
+                    radial-gradient(circle at 70% 30%, rgba(255, 182, 193, 0.4), transparent 60%)`,
+                }}
+            />
+            <div className="animate-in zoom-in duration-300 ease-in-out">
+                <Loader2 size={32} className="text-secondary animate-spin" />
+            </div>
+        </div>
+    );
+}
+
+export default LoadingScreen;
