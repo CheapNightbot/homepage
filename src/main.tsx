ca1ce8fc6ae0e34e908de92c5331b81684c653b5
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-06 10:17:29 +0900

    Add Right Click Context Menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,33 @@
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
+import { FileCode, RefreshCw } from "lucide-react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { SOURCE_CODE } from "./constants.ts"
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="min-h-screen bg-cover bg-center bg-no-repeat relative overflow-hidden bg-[url('/src/assets/imgs/background.jpg')] animate-in fade-in zoom-in-105 scale-110 duration-1000 ease-in-out"></div>
-    <App />
-  </StrictMode>,
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <div className="min-h-screen bg-cover bg-center bg-no-repeat relative overflow-hidden bg-[url('/src/assets/imgs/background.jpg')] animate-in fade-in zoom-in-105 scale-110 duration-1000 ease-in-out"></div>
+        <App />
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem onClick={() => location.reload()}>
+          <RefreshCw />
+          Refresh
+        </ContextMenuItem>
+        <ContextMenuItem onClick={() => window.open(SOURCE_CODE, "_blank")}>
+          <FileCode />
+          Source Code
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
+  </StrictMode>
 )
