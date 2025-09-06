2166ec66241a090cfa3b9f978db961446e101069
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-06 12:10:37 +0900

    Update Loading Screen to at least show once

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,30 @@ import LoadingScreen from "./components/loading-screen";
 
 function App() {
     const [loading, setLoading] = useState(true);
+    const [showContent, setShowContent] = useState(false);
 
     useEffect(() => {
-        const handleLoad = () => setLoading(false);
-        window.addEventListener("load", handleLoad);
+        const handleLoad = () => {
+            if (window.sessionStorage.getItem("hasShownLoading")) {
+                setLoading(false);
+                setShowContent(true);
+                return;
+            }
+
+            setTimeout(() => {
+                setLoading(false);
+                setTimeout(() => {
+                    setShowContent(true);
+                    window.sessionStorage.setItem("hasShownLoading", "true");
+                });
+            }, 1000);
+        };
+
+        if (document.readyState === "complete") {
+            handleLoad();
+        } else {
+            window.addEventListener("load", handleLoad);
+        }
 
         return () => { window.removeEventListener("load", handleLoad) };
     }, []);
@@ -18,7 +38,7 @@ function App() {
         <ThemeProvider defaultTheme="dark" storageKey="potato-ui-theme">
             {loading && <LoadingScreen />}
             {
-                !loading &&
+                showContent &&
                 <>
                     <div className="min-h-screen bg-cover bg-center bg-no-repeat relative overflow-hidden bg-[url('/src/assets/imgs/background.jpg')] animate-in fade-in zoom-in-105 scale-110 duration-1000 ease-in-out"></div>
                     <main className="w-screen h-screen">
