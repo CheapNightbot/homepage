0f648129c3002f346e629135c9c863e11bd394c9
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-06 09:01:09 +0900

    Add Wallpaper/Background Image

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <div className="min-h-screen bg-cover bg-center bg-no-repeat relative overflow-hidden bg-[url('/src/assets/imgs/background.jpg')] animate-in fade-in zoom-in-105 scale-110 duration-1000 ease-in-out"></div>
     <App />
   </StrictMode>,
 )
