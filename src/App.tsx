ecbf8967ad2c213dcde6d245858b62bc3c09dac7
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-07 09:09:02 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
             {
                 showContent &&
                 <>
-                    <div className="min-h-screen bg-cover bg-center bg-no-repeat relative overflow-hidden bg-[url('/src/assets/imgs/background.jpg')] animate-in fade-in zoom-in-105 scale-110 duration-1000 ease-in-out"></div>
+                    <div className="wallpaper"></div>
                     <main className="w-screen h-screen">
                         <TopBar />
                         <Shelf />
