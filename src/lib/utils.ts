2d21e2cd2d33868a926d71ac6612665c305dc504
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-20 00:09:28 +0900

    Add helper func to copy text to clipboard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { toast } from "sonner"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function copyToClipboard(
+  text: string,
+  successMsg = "Text copied to the clipboard!",
+  errorMsg = "Something went wrong trying to copy text to the clipboard!"
+) {
+  try {
+    await navigator.clipboard.writeText(text)
+      .then(() => {
+        toast.success(successMsg);
+      });
+  } catch (error) {
+    toast.error(errorMsg);
+  }
 }
