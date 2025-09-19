59ab34cde640ed44ba28a01d6f1483b91aa5af3c
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-19 20:52:00 +0900

    Update top-bar.tsx

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -3,7 +3,7 @@ import { SITE_TITLE } from "@/constants";
 
 function TopBar() {
     return (
-        <header className="fixed top-0 left-0 w-full h-8 bg-card/20 backdrop-blur-md flex drop-shadow-md items-center justify-between px-4 z-10">
+        <header className="w-full h-8 bg-card/20 backdrop-blur-md flex drop-shadow-md items-center justify-between px-4 z-10">
             <h1 className="text-sm font-semibold text-white/85 hover:text-white select-none transition-all duration-300 ease-in-out">{SITE_TITLE}</h1>
             <ModeToggle />
         </header>
