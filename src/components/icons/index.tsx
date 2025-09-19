faf826e456c6abc4cae5ac12b293978c99d49c85
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-20 02:35:56 +0900

    Add Social Icons Component

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,13 @@
+import Bluesky from "./bluesky";
+import Discord from "./discord";
+import Github from "./github";
+import Mastodon from "./mastodon";
+import TwitterX from "./twitter_x";
+
+export {
+    Bluesky,
+    Discord,
+    Github,
+    Mastodon,
+    TwitterX
+};
