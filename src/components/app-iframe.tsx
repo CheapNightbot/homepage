1cb48e0bac1b8c44c62f76c3c84f5cec7c13b9be
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-20 06:50:47 +0900

    Create app-iframe.tsx

---
+++ src/components/app-iframe.tsx
@@ -0,0 +1,41 @@
+import { Progress } from "@/components/ui/progress";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+
+function AppIframe({ src }: { src: string }) {
+    const [loadingIframe, setLoadingIframe] = useState(true);
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        if (loadingIframe) {
+            const intervalId = setInterval(() => {
+                setProgress((prev) => (prev < 90 ? prev + 10 : prev));
+            }, 100);
+            return () => clearInterval(intervalId);
+        }
+    }, [loadingIframe]);
+
+    const handleLoad = () => {
+        setProgress(100);
+        setTimeout(() => setLoadingIframe(false), 500);
+    }
+
+    return (
+        <>
+            {loadingIframe && (
+                <div className="relative w-full h-full flex items-center justify-center">
+                    <Progress value={progress} className={cn("w-full rounded-none absolute top-0 left-0", !loadingIframe && "animate-out blur-out duration-300")} />
+                    <Loader2 size={50} className={cn(loadingIframe ? "animate-spin" : "animate-out blur-out duration-300")} />
+                </div>
+            )}
+            <iframe
+                onLoad={handleLoad}
+                src={src}
+                className={cn("w-full h-full invisible", !loadingIframe && "visible animate-in fade-in duration-300 ease-in")}
+            />
+        </>
+    );
+}
+
+export default AppIframe;
