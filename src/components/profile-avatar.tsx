2765466620eaa0b1661836c1833acf0e15bdaa79
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-20 02:39:13 +0900

    Create profile-avatar.tsx

---
+++ src/components/profile-avatar.tsx
@@ -0,0 +1,23 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import CheapNightbot from "../assets/imgs/Cheap Nightbot.svg";
+import { useState } from "react";
+import { VenusMarsFlip } from "./VenusMarsFlip";
+
+function ProfileAvatar() {
+    const [showIcon, setShowIcon] = useState(false);
+
+    return (
+        <Avatar onClick={() => setShowIcon(!showIcon)} className="size-26 transition-all duration-300 ease-in-out active:scale-95">
+            {
+                showIcon ?
+                    <div className="bg-gradient-to-b from-[#55CDFC] via-[#FFFFFF] to-[#F7A8B8] flex size-full items-center justify-center rounded-full animate-in blur-in-md duration-200">
+                        <VenusMarsFlip size={50} />
+                    </div>
+                    : <AvatarImage src={CheapNightbot} alt="Cheap Nightbot" />
+            }
+            <AvatarFallback className="text-lg">/ᐠ - ˕ -マ</AvatarFallback>
+        </Avatar>
+    );
+}
+
+export default ProfileAvatar;
