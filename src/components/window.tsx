259e610a05e3f0c5e176d08f399bce7fba0ebbb0
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-20 00:47:56 +0900

    Some Refactoring ~

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -1,49 +1,52 @@
 import { Circle, Minus, Plus, X } from 'lucide-react';
 import { Rnd } from 'react-rnd';
 import { Button } from './ui/button';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { cn } from '@/lib/utils';
 
-function Window({ title, children, className }) {
-    const [winWidth, setWinWidth] = useState(480);
-    const [winHeight, setWinHeight] = useState(540);
+interface WindowProps {
+    title: string,
+    children: any,
+    className: string
+};
+
+// 32px - the height of top bar !
+// 80px - the height of shelf at bottom !
+// 32 + 80 = 112 + 44 = 144
+const BOTTOM_PADDING = 156;
+const INIT_WIDTH = 480;
+const INIT_HEIGHT = 540;
+
+function Window({ title, children, className }: WindowProps) {
+    const [winWidth, setWinWidth] = useState(INIT_WIDTH);
+    const [winHeight, setWinHeight] = useState(INIT_HEIGHT);
     const [posX, setPosX] = useState((window.innerWidth - winWidth) / 2);
-    const [posY, setPosY] = useState((window.innerHeight - winHeight) / 2);
+    const [posY, setPosY] = useState((window.innerHeight - (winHeight + 120)) / 2);
 
     const [maximized, setMaximized] = useState(false);
     const [allowTransitions, setAllowTransitions] = useState(true);
 
     const handleMaximize = () => {
+        setAllowTransitions(true);
         if (!maximized) {
-            setWinWidth(window.innerWidth);
-            setWinHeight(window.innerHeight - 32); // 32px - the height of top bar !
-            setPosX(0);
-            setPosY(0);
+            setWinWidth(window.innerWidth - 32);
+            setWinHeight(window.innerHeight - BOTTOM_PADDING);
+            setPosX(16);
+            setPosY(16);
         } else {
-            setWinWidth(480);
-            setWinHeight(540);
-            setPosX((window.innerWidth - 480) / 2);
-            setPosY((window.innerHeight - 540) / 2);
+            setWinWidth(INIT_WIDTH);
+            setWinHeight(INIT_HEIGHT);
+            setPosX((window.innerWidth - INIT_WIDTH) / 2);
+            setPosY((window.innerHeight - (INIT_HEIGHT + 120)) / 2);
         }
         setMaximized(!maximized);
     }
-
-    // Resize handling for the browser window itself, not the Rnd window !
-    const handleResize = () => {
-        setPosX((window.innerWidth - winWidth) / 2);
-        setPosY((window.innerHeight - winHeight) / 2);
-    };
-
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
 
     return (
         <Rnd
             bounds="parent"
             cancel='.actions'
-            className={cn('overflow-clip flex flex-col', maximized ? 'z-100' : 'rounded-2xl', allowTransitions && 'transition-all duration-300 ease-in-out')}
+            className={cn('overflow-clip flex flex-col rounded-2xl', allowTransitions && 'transition-all duration-300 ease-in-out')}
             default={{
                 x: posX,
                 y: posY,
@@ -53,22 +56,26 @@ function Window({ title, children, className }) {
             dragHandleClassName='nav-bar'
             onResizeStart={() => setAllowTransitions(false)}
             onResizeStop={(_, __, ___, delta, ____) => {
-                setWinWidth((prev) => prev + delta.width);
-                setWinHeight((prev) => prev + delta.height);
-                setAllowTransitions(true);
+                if (!maximized) {
+                    setWinWidth((prev) => prev + delta.width);
+                    setWinHeight((prev) => prev + delta.height);
+                    setAllowTransitions(true);
+                }
             }}
             onDragStart={() => setAllowTransitions(false)}
             onDragStop={(_, data) => {
-                setPosX(data.x);
-                setPosY(data.y);
-                setAllowTransitions(true);
+                if (!maximized) {
+                    setPosX(data.x);
+                    setPosY(data.y);
+                    setAllowTransitions(true);
+                }
             }}
             minWidth={460}
             minHeight={540}
             position={{ x: posX, y: posY }}
             size={{ width: winWidth, height: winHeight }}
         >
-            <header onDoubleClick={handleMaximize} className='nav-bar bg-card px-4 h-8 flex justify-between items-center'>
+            <header onContextMenu={(uwu) => uwu.preventDefault()} onDoubleClick={handleMaximize} className='nav-bar bg-card px-4 h-8 flex justify-between items-center'>
                 <h2 className='text-sm'>{title || 'Title'}</h2>
                 <nav className='actions group flex items-center gap-2 text-background'>
                     {/* maximize button */}
@@ -88,7 +95,7 @@ function Window({ title, children, className }) {
                     </Button>
                 </nav>
             </header>
-            <div className={cn('w-full h-full border p-12 bg-card/50 text-card-foreground backdrop-blur-3xl', className)}>
+            <div onContextMenu={(uwu) => uwu.preventDefault()} className={cn('w-full h-full border p-12 bg-card/50 text-card-foreground backdrop-blur-3xl', className)}>
                 {children}
             </div>
         </Rnd>
