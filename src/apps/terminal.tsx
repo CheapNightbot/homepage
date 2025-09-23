489a484309574bc50a29b3fc53471662977bfae1
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-24 01:59:34 +0900

    Update terminal.tsx
    - IT'S GRASS AT THIS POINT WWW, no maybe FOREST !!!!

--- src/apps/terminal.tsx
+++ src/apps/terminal.tsx
@@ -4,33 +4,68 @@ import Window from "@/components/window";
 import { ChevronsRight } from "lucide-react";
 import { useState } from "react";
 
-
 export default function Terminal() {
     const [cmd, setCmd] = useState("");
-    const [topOffset, setTopOffset] = useState(0);
+    const [cmdHistory, setCmdHistory] = useState([""]);
+
+    // ------- Terminal Command Handlers ---------
+    // `clear` command
+    const handleClear = () => {
+        setCmdHistory([""]);
+    }
+
+    // `echo` command
+    const handleEcho = () => {
+        const startQuoteIdx = cmd.indexOf('"') + 1;
+        const endQuoteIdx = cmd.indexOf('"', startQuoteIdx);
+
+        alert(cmd.slice(startQuoteIdx, endQuoteIdx));
+    }
+
+    // --------------------------------------------
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setCmd("");
+        setCmdHistory((prev) => [...prev, cmd]);
 
-        if (cmd === "clear") {
-            setTopOffset(0);
+        const cmdHandler = CMD_LIST.find((c) => c.name === cmd);
+        if (cmdHandler) {
+            cmdHandler.cmd();
         } else {
-            setTopOffset((prev) => prev + 10);
+            cmd.trim().length > 0 && setCmdHistory([...cmdHistory, `${cmd}: command not found!`])
+        }
+        setCmd("");
+    }
+
+    const handleCmdHistoryNav = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "ArrowUp") {
+            cmdHistory.length > 0 && setCmd(cmdHistory[0]);
         }
     }
+
+    const CMD_LIST = [
+        { name: "clear", cmd: handleClear },
+        { name: "echo", cmd: handleEcho },
+    ]
 
     return (
         <Window title="Terminal" contentClassName="px-2">
             <ScrollArea
                 onClick={() => document.getElementById("cmd-inpt")?.focus()}
                 className="w-full h-full">
+
+                {cmdHistory &&
+                    <section>
+                        {cmdHistory.map((cmd, i) => <p className="text-foreground/80 min-h-[1ch]" key={i}>{cmd}</p>)}
+                    </section>
+                }
                 <form
                     onSubmit={(e) => handleSubmit(e)}
                 >
-                    <fieldset style={{ marginTop: topOffset }} className="flex items-center gap-0.5">
+                    <fieldset className="flex items-center gap-0.5">
                         <ChevronsRight />
                         <Input
+                            onKeyDown={(e) => handleCmdHistoryNav(e)}
                             id="cmd-inpt"
                             value={cmd}
                             onChange={(e) => setCmd(e.currentTarget.value)}
