def1062c74cae37c8bc8be699651ec2763f6bc2f
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-25 08:41:07 +0900

    Update top-bar.tsx

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -5,10 +5,12 @@ import { FullScreenToggle } from "./full-screen-toggle";
 function TopBar() {
     return (
         <header className="w-full h-8 bg-card/20 backdrop-blur-md flex drop-shadow-md items-center justify-between px-4 z-10">
-            <h1 className="text-sm font-semibold text-white/85 hover:text-white select-none transition-all duration-300 ease-in-out">{SITE_TITLE}</h1>
+            <h1 className="text-sm font-semibold text-white/85 hover:text-white select-none transition-all duration-300 ease-in-out">
+                {SITE_TITLE}
+            </h1>
             <div className="flex items-center gap-4">
-                <FullScreenToggle />
                 <ModeToggle />
+                <FullScreenToggle />
             </div>
         </header>
     );
