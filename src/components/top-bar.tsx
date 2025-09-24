2edadacf7a63205f9be61a5967c800a76d0278dc
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-09-25 08:51:21 +0900

    🍥 🤤

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,13 +1,17 @@
 import { ModeToggle } from "@/components/mode-toggle"
 import { SITE_TITLE } from "@/constants";
 import { FullScreenToggle } from "./full-screen-toggle";
+import { Shell } from "lucide-react";
 
 function TopBar() {
     return (
         <header className="w-full h-8 bg-card/20 backdrop-blur-md flex drop-shadow-md items-center justify-between px-4 z-10">
-            <h1 className="text-sm font-semibold text-white/85 hover:text-white select-none transition-all duration-300 ease-in-out">
-                {SITE_TITLE}
-            </h1>
+            <div className="flex items-center gap-2">
+                <Shell size={18} className="animate-in spin-in-[360deg] fade-in duration-500 ease-in" />
+                <h1 className="text-sm font-semibold text-white/85 hover:text-white select-none transition-all duration-300 ease-in-out">
+                    {SITE_TITLE}
+                </h1>
+            </div>
             <div className="flex items-center gap-4">
                 <ModeToggle />
                 <FullScreenToggle />
