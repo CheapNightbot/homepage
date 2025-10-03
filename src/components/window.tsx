3a6a9c08127e8e034dc452358c66242de605f55c
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-10-04 02:23:47 +0900

    Fix Transition on close/minimize
    - before it was going to far left side.

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -109,128 +109,132 @@ function Window({
     }, [isMaximized, width, height, mainElm]);
 
     return (
-        <Rnd
-            onClick={() => focusWindow(windowId)}
-            windowid={windowId}
-            bounds="#main"
-            cancel='.actions'
-            className={cn(
-                'overflow-clip flex flex-col rounded-2xl',
-                allowTransitions && 'transition-all duration-300 ease-in-out',
-                isFocused && 'z-10',
-                // showOpenAnim && 'animate-in duration-300 slide-in-from-bottom zoom-in fade-in blur-in ease-in',
-                isMinimized || windowClosing ? 'animate-out duration-300 slide-out-to-bottom-160 zoom-out fade-out blur-out ease-out animate-stay' : '',
-                className
-            )}
-            default={{
-                x: posX,
-                y: posY,
-                width: winWidth,
-                height: winHeight,
-            }}
-            dragHandleClassName='nav-bar'
-            disableDragging={isMaximized}
-            enableResizing={!isMaximized}
-            onResizeStart={() => setAllowTransitions(false)}
-            onResizeStop={(_, __, ___, delta, ____) => {
-                if (!isMaximized) {
-                    setWinWidth((prev) => prev + delta.width);
-                    setWinHeight((prev) => prev + delta.height);
-                    setAllowTransitions(true);
-                }
-            }}
-            onDragStart={() => setAllowTransitions(false)}
-            onDragStop={(_, data) => {
-                if (!isMaximized) {
-                    setPosX(data.x);
-                    setPosY(data.y);
-                    setAllowTransitions(true);
-                }
-            }}
-            minWidth={460}
-            minHeight={540}
-            position={{ x: posX, y: posY }}
-            size={{ width: winWidth, height: winHeight }}
-        >
-            <header
-                onContextMenu={(uwu) => uwu.preventDefault()}
-                onDoubleClick={handleMaximize}
+        <div className={cn(
+            isMinimized || windowClosing ? 'animate-out duration-300 slide-out-to-bottom-220 zoom-out fade-out blur-out ease-out animate-stay' : '',
+        )}>
+            <Rnd
+                onClick={() => focusWindow(windowId)}
+                windowid={windowId}
+                bounds="#main"
+                cancel='.actions'
                 className={cn(
-                    'nav-bar bg-card px-4 h-8 flex justify-between items-center select-none gap-2',
-                    !isFocused && 'saturate-50'
+                    'overflow-clip flex flex-col rounded-2xl',
+                    allowTransitions && 'transition-all duration-300 ease-in-out',
+                    isFocused && 'z-10',
+                    // showOpenAnim && 'animate-in duration-300 slide-in-from-bottom zoom-in fade-in blur-in ease-in',
+                    // isMinimized || windowClosing ? 'animate-out duration-300 slide-out-to-bottom-160 zoom-out fade-out blur-out ease-out animate-stay' : '',
+                    className
                 )}
+                default={{
+                    x: posX,
+                    y: posY,
+                    width: winWidth,
+                    height: winHeight,
+                }}
+                dragHandleClassName='nav-bar'
+                disableDragging={isMaximized}
+                enableResizing={!isMaximized}
+                onResizeStart={() => setAllowTransitions(false)}
+                onResizeStop={(_, __, ___, delta, ____) => {
+                    if (!isMaximized) {
+                        setWinWidth((prev) => prev + delta.width);
+                        setWinHeight((prev) => prev + delta.height);
+                        setAllowTransitions(true);
+                    }
+                }}
+                onDragStart={() => setAllowTransitions(false)}
+                onDragStop={(_, data) => {
+                    if (!isMaximized) {
+                        setPosX(data.x);
+                        setPosY(data.y);
+                        setAllowTransitions(true);
+                    }
+                }}
+                minWidth={460}
+                minHeight={540}
+                position={{ x: posX, y: posY }}
+                size={{ width: winWidth, height: winHeight }}
             >
-                <h2 className={cn(
-                    'text-sm transition-all duration-300 ease-in-out',
-                    !isFocused && 'text-foreground/80'
-                )}
+                <header
+                    onContextMenu={(uwu) => uwu.preventDefault()}
+                    onDoubleClick={handleMaximize}
+                    className={cn(
+                        'nav-bar bg-card px-4 h-8 flex justify-between items-center select-none gap-2',
+                        !isFocused && 'saturate-50'
+                    )}
                 >
-                    {title || 'Title'}
-                </h2>
-                {
-                    contextMenu &&
-                    <ContextMenu >
-                        <ContextMenuTrigger className='flex-1 h-full' />
-                        <ContextMenuContent>
-                            <ContextMenuItem onClick={() => window.open(contextMenu.source, "_blank")}>
-                                <FileCode />
-                                Source Code
-                            </ContextMenuItem>
-                            <ContextMenuItem onClick={() => window.open(contextMenu.live, "_blank")}>
-                                <SquareArrowOutUpRight />
-                                Open in New Tab
-                            </ContextMenuItem>
-                        </ContextMenuContent>
-                    </ContextMenu>
-                }
-                <nav className='actions group flex items-center gap-2 text-background'>
-                    {/* maximize button */}
-                    <Button
-                        onClick={handleMaximize}
-                        variant="ghost"
-                        size="icon"
-                        className='relative rounded-full size-4 active:scale-95'>
-                        {isFocused
-                            ? <Circle fill='green' stroke='none' className="transition-all duration-300 ease-in-out" />
-                            : <Circle fill='gray' stroke='none' className="transition-all duration-300 ease-in-out" />
-                        }
-                        <Plus className='absolute size-2.5 left-1/2 top-1/2 -translate-1/2 border-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ' />
-                    </Button>
-                    {/* minimize button */}
-                    {canMinimize &&
+                    <h2 className={cn(
+                        'text-sm transition-all duration-300 ease-in-out',
+                        !isFocused && 'text-foreground/80'
+                    )}
+                    >
+                        {title || 'Title'}
+                    </h2>
+                    {
+                        contextMenu &&
+                        <ContextMenu >
+                            <ContextMenuTrigger className='flex-1 h-full' />
+                            <ContextMenuContent>
+                                <ContextMenuItem onClick={() => window.open(contextMenu.source, "_blank")}>
+                                    <FileCode />
+                                    Source Code
+                                </ContextMenuItem>
+                                <ContextMenuItem onClick={() => window.open(contextMenu.live, "_blank")}>
+                                    <SquareArrowOutUpRight />
+                                    Open in New Tab
+                                </ContextMenuItem>
+                            </ContextMenuContent>
+                        </ContextMenu>
+                    }
+                    <nav className='actions group flex items-center gap-2 text-background'>
+                        {/* maximize button */}
                         <Button
-                            onClick={() => minimizeWindow(windowId)}
+                            onClick={handleMaximize}
                             variant="ghost"
                             size="icon"
                             className='relative rounded-full size-4 active:scale-95'>
                             {isFocused
-                                ? <Circle fill='orange' stroke='none' className="transition-all duration-300 ease-in-out" />
+                                ? <Circle fill='green' stroke='none' className="transition-all duration-300 ease-in-out" />
                                 : <Circle fill='gray' stroke='none' className="transition-all duration-300 ease-in-out" />
                             }
-                            <Minus className='absolute size-2.5 left-1/2 top-1/2 -translate-1/2 border-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ' />
+                            <Plus className='absolute size-2.5 left-1/2 top-1/2 -translate-1/2 border-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ' />
                         </Button>
-                    }
-                    {/* close button */}
-                    <Button onClick={handleClose}
-                        variant="ghost"
-                        size="icon"
-                        className='relative rounded-full size-4 active:scale-95'>
-                        {isFocused
-                            ? <Circle fill='red' stroke='none' className="transition-all duration-300 ease-in-out" />
-                            : <Circle fill='gray' stroke='none' className="transition-all duration-300 ease-in-out" />
+                        {/* minimize button */}
+                        {canMinimize &&
+                            <Button
+                                onClick={() => minimizeWindow(windowId)}
+                                variant="ghost"
+                                size="icon"
+                                className='relative rounded-full size-4 active:scale-95'>
+                                {isFocused
+                                    ? <Circle fill='orange' stroke='none' className="transition-all duration-300 ease-in-out" />
+                                    : <Circle fill='gray' stroke='none' className="transition-all duration-300 ease-in-out" />
+                                }
+                                <Minus className='absolute size-2.5 left-1/2 top-1/2 -translate-1/2 border-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ' />
+                            </Button>
                         }
-                        <X className='absolute size-2.5 left-1/2 top-1/2 -translate-1/2 border-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ' />
-                    </Button>
-                </nav>
-            </header>
-            <main
-                onContextMenu={(uwu) => uwu.preventDefault()}
-                className={cn(
-                    'w-full h-[calc(100%-32px)] rounded-b-2xl text-card-foreground bg-card/50 backdrop-blur-3xl',
-                    contentClassName)}>
-                {children}
-            </main>
-        </Rnd>
+                        {/* close button */}
+                        <Button onClick={handleClose}
+                            variant="ghost"
+                            size="icon"
+                            className='relative rounded-full size-4 active:scale-95'>
+                            {isFocused
+                                ? <Circle fill='red' stroke='none' className="transition-all duration-300 ease-in-out" />
+                                : <Circle fill='gray' stroke='none' className="transition-all duration-300 ease-in-out" />
+                            }
+                            <X className='absolute size-2.5 left-1/2 top-1/2 -translate-1/2 border-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ' />
+                        </Button>
+                    </nav>
+                </header>
+                <main
+                    onContextMenu={(uwu) => uwu.preventDefault()}
+                    className={cn(
+                        'w-full h-[calc(100%-32px)] rounded-b-2xl text-card-foreground bg-card/50 backdrop-blur-3xl',
+                        contentClassName)}>
+                    {children}
+                </main>
+            </Rnd>
+        </div>
     );
 }
 
