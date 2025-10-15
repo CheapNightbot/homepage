7ba7dcc839421081373922df4fee1aed52d0f7f8
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-10-15 17:02:06 +0900

    Add Border on the top bar if there is no wallpaer

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -11,7 +11,10 @@ function TopBar() {
     return (
         <header
             onContextMenu={(uwu) => uwu.preventDefault()}
-            className="w-full h-8 bg-card/20 backdrop-blur-md flex drop-shadow-md items-center justify-between px-4 z-10">
+            className={cn(
+                'w-full h-8 bg-card/20 backdrop-blur-md flex drop-shadow-md items-center justify-between px-4 z-10',
+                currentWallpaper === "#" && 'border-b'
+            )}>
             <div className="flex items-center gap-2">
                 <Shell size={18} className={cn(
                     "animate-in spin-in-[360deg] fade-in duration-500 ease-in",
