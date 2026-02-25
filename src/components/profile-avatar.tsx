e49af3e9f356b74347c4eea6859254c138ef8b79
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-02-25 19:54:10 +0900

    Prevent Image drag using mouse

--- src/components/profile-avatar.tsx
+++ src/components/profile-avatar.tsx
@@ -10,10 +10,10 @@ function ProfileAvatar() {
         <Avatar onClick={() => setShowIcon(!showIcon)} className="select-none size-26 transition-all duration-300 ease-in-out active:scale-95">
             {
                 showIcon ?
-                    <div className="bg-gradient-to-b from-[#55CDFC] via-[#FFFFFF] to-[#F7A8B8] flex size-full items-center justify-center rounded-full animate-in blur-in-md duration-200">
+                    <div className="bg-linear-to-b from-[#55CDFC] via-[#FFFFFF] to-[#F7A8B8] flex size-full items-center justify-center rounded-full animate-in blur-in-md duration-200">
                         <VenusMarsFlip size={50} />
                     </div>
-                    : <AvatarImage src={CheapNightbot} alt="Cheap Nightbot" className="animate-in fade-in duration-300 ease-in-out" />
+                    : <AvatarImage src={CheapNightbot} alt="Cheap Nightbot" className="animate-in fade-in duration-300 ease-in-out pointer-events-none" />
             }
             {
                 !showIcon &&
